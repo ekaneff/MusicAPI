a4a4063a7eb17cd421bd640fe78fc1d0f420d8b6
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-14 14:33:23 -0400

    added gulp files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+var argv = require('yargs').argv;
+var gitignore = require('gulp-gitignore');
+
+gulp.task('add', function() {
+	console.log('adding...')
+	return gulp.src('.')
+		.pipe(gitignore())
+		.pipe(git.add());
+});
+
+gulp.task('commit', function() {
+  console.log('commiting...');
+  if (argv.m) {
+    return gulp.src('.')
+      .pipe(git.commit(argv.m));
+  }
+});
+
+gulp.task('push', function(){
+  console.log('pushing...');
+  git.push('origin', argv.b, function (err) {
+    if (err) throw err;
+  });
+});
+
+gulp.task('git', ['add', 'commit']);
+
+gulp.task('send', ['add', 'commit', 'push']);
